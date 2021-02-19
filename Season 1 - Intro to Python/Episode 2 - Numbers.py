826be51d8c7a870a90e74d5fe87f0ba97a1fe00d
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-02-19 20:09:16 +0530

    Ep.2 - Numbers

---
+++ Season 1 - Intro to Python/Episode 2 - Numbers.py
@@ -0,0 +1,18 @@
+# 2. Numbers
+
+age = 35  # Integer
+PI = 3.14159  # Float & Constant(Variables to be represented in Block letters)
+
+# Now we'll divide numbers to check for optimal solution
+F_divison = 12/4
+print(F_divison)  # Output - 3.0 = one division operator will give a float.
+F_divison = 12//4
+print(F_divison)  # Output - 3 = two division operator will give an integer.
+
+# Remainder of a division
+x = 37
+rem = x % 2
+print(rem)  # Output - 1
+# e.g: A table with gray row after every one line
+#     if odd, set background = grey
+#     if even, set background = white
