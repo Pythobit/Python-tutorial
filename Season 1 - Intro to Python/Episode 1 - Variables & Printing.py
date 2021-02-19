e27a5fe7f4c34779c42b8216464948195b967439
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-02-19 19:44:57 +0530

    Ep.1 - Variables & Printing

---
+++ Season 1 - Intro to Python/Episode 1 - Variables & Printing.py
@@ -0,0 +1,19 @@
+# Hash(#) is used to comment lines in python
+# 1. Variables & Printing
+
+age=30     #Variable
+print(age) #Output - 30
+
+print(30)  #we can simply print a number by writing it inside print statement #Output - 30
+
+age=20
+print(age)  #Output - 20
+age=30      #changing the value of the variable will not update previous variable value
+print(age)  #Output - 30
+
+F_age=30    # Variables cannot start with a number
+# Underscore to differentiate b/w variables, so we can add numbers too, nothing else.
+
+# Using underscores between words gives snake_case
+# Using capital letters between words gives Camelcase
+# Constant variables to be written in Block e.g: PI=3.14159
