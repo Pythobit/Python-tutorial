b1c58ff372a82f28883467ee14405d51682f151e
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-02-22 18:39:55 +0530

    Ep.13 - Join

---
+++ Season 1 - Intro to Python/Episode 13 - Join.py
@@ -0,0 +1,12 @@
+# 13. Join
+
+# it allows to print list a bit better
+
+friends = ['Pythobit','boy','Pythoman']
+print(f'My friends are {friends}.')     # Output - My friends are ['Pythobit', 'boy', 'Pythoman'].
+# So, the Output needs to be a bit clearer.
+
+friends = ['Pythobit','boy','Pythoman']
+friend = ', '.join(friends)
+print(f'My friends are {friend}')       # Output - My friends are Pythobit, boy, Pythoman
+# Here (, ) comma n space is used as separator, but you can use anything.
