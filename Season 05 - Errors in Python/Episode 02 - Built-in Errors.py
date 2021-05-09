0736c8559a32980f1a5703b6600136a577ec5d32
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-05-09 21:01:47 +0530

    Ep. 02 - Built-in Errors

--- Season 05 - Errors in Python/Episode 02 - Built-in Errors.py
+++ Season 05 - Errors in Python/Episode 02 - Built-in Errors.py
@@ -1,1 +1,30 @@
+# Built-in errors.
+"""
+I'm gonna tell you the types of errors mostly occurs in the python, so when they appear you know, what they mean,
+why they occur, and why they appear, so i'll discussing about these errors, so you won't have to waste your googling
+everything.
+
+ 1. IndexError
+ 2. KeyError
+ 3. NameError
+ Errors (1-3) happens when you access something that doesn't have to access in the wrong way.
+
+ 4. AttributeError
+ This error is fairly related to above errors.
+
+ 5. NotImplementedError
+ This error can be used very handy.
+
+ 6. RuntimeError or GenericError
+ 7. SyntaxError
+ Errors (6-7) occurs when you mess with syntaxes in python code.
+
+"""
+
+
+
+
+
+
+
 Ep. 02 - Built-in Errors
