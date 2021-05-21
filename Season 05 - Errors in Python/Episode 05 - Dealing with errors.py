90d812513a7c5122829f6baf046ae8a87ca9ac8b
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-05-21 17:25:59 +0530

    Ep. 05 - Dealing with errors

--- Season 05 - Errors in Python/Episode 05 - Dealing with errors.py
+++ Season 05 - Errors in Python/Episode 05 - Dealing with errors.py
@@ -1,3 +1,43 @@
+# Dealing with errors in Python
 
+# Normal Usage of two classes.
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def __repr__(self):
+        return f'< Car {self.make} {self.model} >'
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def add_car(self, car):
+        self.cars.append(car)
+
+ford = Garage()
+fiesta = Car('Ford', 'Fiesta')
+ford.add_car(fiesta)
+"""
+Normally, we want to make sure that we're adding only cars and nothing else, so what we should do is,
+   
+    if isinstance(fiesta, Car):
+        ford.add_car(fiesta)
+    else:
+        print('Your car was not a car..!')
+    
+    from <line 24> onwards
+    
+instead we can do this at the method add_car
+"""
+def add_car(self, car):
+    if not isinstance(car, Car):
+        raise TypeError('Error..!')
+    else:
+        self.cars.append(car)
 
 Ep. 05 - Dealing with errors
