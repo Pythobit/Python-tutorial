03b930577e3c00acfdc39be9f315b6c9582ebabb
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-06-02 01:43:39 +0530

    Ep. 05 - JSON Files in Python

---
+++ Season 06 - Files in Python/Episode 05 - JSON Files in python.py/Episode 05 - JSON Files.py
@@ -0,0 +1,29 @@
+# JSON (Javascript Object Notation) files in Python
+
+import json
+
+file = open('friends_json.txt', 'r')
+file_content = json.load(file)
+file.close()
+
+print(file_content['friends'][0])
+
+
+cars = [
+    {
+        'make': 'Ford', 'model': 'Fiesta'
+    },
+    {
+        'make': 'Ford', 'model': 'Focus'
+    }
+]
+
+file = open('cars_json.txt', 'w')
+json.dump(cars, file)
+file.close()
+
+
+my_json_string = '[{"name": "Alfa Romeo", "released": 1950}]'
+
+incorrect_car = json.loads(my_json_string)
+print(incorrect_car)
