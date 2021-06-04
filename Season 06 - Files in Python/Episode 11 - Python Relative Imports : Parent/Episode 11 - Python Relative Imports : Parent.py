3e57c7641457f10f0cfb11adca74cb40e5c39ed9
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-06-05 01:09:27 +0530

    Ep. 11 - Python Relative Imports : Parent

--- Season 06 - Files in Python/Episode 11 - Python Relative Imports : Parent/Episode 11 - Python Relative Imports : Parent.py
+++ Season 06 - Files in Python/Episode 11 - Python Relative Imports : Parent/Episode 11 - Python Relative Imports : Parent.py
@@ -7,3 +7,4 @@ if you want to import the parent, we need to use two dots instead of one as show
 from ..find import NotFoundError
 # ---^^ these two dots tells python to go the main import directory from where the project starts (i.e: utils)
 
+
