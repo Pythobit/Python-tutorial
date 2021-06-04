c85fb8cdf6ac9b565fb3d9088907eb6c19bdef24
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-06-05 01:19:59 +0530

    Ep. 12 - Imports Error & running as Python Scripts

---
+++ Season 06 - Files in Python/Episode 12 - Imports Error & running as Python Scripts/Episode 12 - Imports Error & running as Python Scripts.py
@@ -0,0 +1,19 @@
+# Imports Error & running as Python Scripts.
+
+"""
+IMPORT ERROR
+
+if your module is already imported, you can import it the same way, but if you want to access something inside in it,
+python will look into the module & give you an error because it's going to be back n forth,
+called `circular import` [bad thing]
+
+"""
+"""
+RUNNING AS PYTHON SCRIPTS
+
+if you want to run a specific code when you run a specific file, do this
+
+if __name__ =  '__main__':
+    print(find_in(['Rolf', 'Jose', 'Jen'], lambda x: x, 'Jose'))
+    
+"""
