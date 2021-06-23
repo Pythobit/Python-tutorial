24d1173c25c8dc68f1af2cca710ebfa2db5319b1
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-06-23 20:27:16 +0530

    Create database.py

---
+++ Season 07 - Databases in Python & Milestone Project 2/Episode 06 - Developing our own Context Managers/utils/database.py
@@ -0,0 +1,45 @@
+from .databaseconnection import DatabaseConnection
+
+"""
+Concerned with storing and returning books from a database file.
+"""
+"""
+Do not ever use a f string in the SQL, as it is dangerous, and can open way to SQL Injections...!!
+"""
+
+
+def create_book_table():
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
+
+
+def add_book(name, author):
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
+
+
+def get_all_books():
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('SELECT * FROM books')
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
+    return books
+
+
+def mark_book_as_read(name):
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
+
+
+def delete_book(name):
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('DELETE FROM books WHERE name=?', (name,))
