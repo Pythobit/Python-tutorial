0b9857d0252e66b6dea76feb4f6d157d651874a9
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-09 14:41:47 +0530

    Create Episode 06 - Queues in Python.py

---
+++ Season 10 - Advanced Python Development/Episode 06 - Queues in Python.py
@@ -0,0 +1,11 @@
+# Queue in python
+
+"""
+Queue is like people entering in a theatre in queue
+i.e: add elements at the end, and remove the elements from the start
+
+A Queue in python is where you can add or remove elements either side is called `deque` or `double ended queue`.
+
+and in the stack, you add and remove from the same end
+
+"""
