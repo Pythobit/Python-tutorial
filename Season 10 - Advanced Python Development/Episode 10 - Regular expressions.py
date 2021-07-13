0a6be57a3d1db8508a277259eb1a52a9679b2121
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-13 13:21:09 +0530

    Create Episode 10 - Regular expressions.py

---
+++ Season 10 - Advanced Python Development/Episode 10 - Regular expressions.py
@@ -0,0 +1,18 @@
+# Regular Expressions in python
+
+"""
+"Getting" regex is a matter of seeing patterns
+and being able to express them using languages
+
+Four most important components:
+[.] period : "anything" - letters, numbers, symbols,.. but no newlines
+[+] plus : "one or more of"
+[*] asterisk : "zero or more of"
+[?] question mark : "zero or one of"
+
+example - jose@teclado.com
+            .*
+            .+
+
+"""
+
