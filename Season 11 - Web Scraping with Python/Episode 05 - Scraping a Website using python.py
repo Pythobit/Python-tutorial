ef599abde7dd0d0017e74a9527f858cd7f06cb09
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-14 15:14:28 +0530

    Create Episode 05 - Scraping a Website using python.py

---
+++ Season 11 - Web Scraping with Python/Episode 05 - Scraping a Website using python.py
@@ -0,0 +1,11 @@
+# Scraping website with python
+
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('http://www.example.com')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.find('h1').string)
+print(soup.select_one('p a').attrs['href'])
+
