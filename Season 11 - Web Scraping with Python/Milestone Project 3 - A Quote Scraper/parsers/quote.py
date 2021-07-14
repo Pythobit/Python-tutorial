70b8dbb2ca4d903f264014154182c3813820f258
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-14 16:10:12 +0530

    Create quote.py

---
+++ Season 11 - Web Scraping with Python/Milestone Project 3 - A Quote Scraper/parsers/quote.py
@@ -0,0 +1,29 @@
+from locators.quote_locators import QuoteLocators
+
+
+class QuoteParser:
+    """
+    Given one of the specific quote divs, find out the data about the quote
+    (content, author, tags).
+    """
+    def __init__(self, parent):
+        self.parent = parent
+
+    def __repr__(self):
+        return f'< Quote : {self.content} by {self.author} >'
+
+    @property
+    def content(self):
+        locator = QuoteLocators.CONTENT
+        return self.parent.select_one(locator).string
+
+    @property
+    def author(self):
+        locator = QuoteLocators.AUTHOR
+        return self.parent.select_one(locator).string
+
+    @property
+    def tags(self):
+        locator = QuoteLocators.TAGS
+        return [e.string for e in self.parent.select(locator).string]
+
