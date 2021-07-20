2bf4c336a254004af95446533ceb361edcc23e48
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-20 15:24:46 +0530

    Create app.py

---
+++ Season 11 - Web Scraping with Python/Milestone Project 4 - Book Scraper/Without Logging/app.py
@@ -0,0 +1,14 @@
+import requests
+
+from pages.all_book_pages import AllBooksPage
+
+page_content = requests.get('http://books.toscrape.com').content
+page = AllBooksPage(page_content)
+
+books = page.books
+
+for page_num in range(1, page.page_count):
+    url = f'http://books.toscrape.com/catalogue/page-{page_num+1}.html'
+    page_content = requests.get(url).content
+    page = AllBooksPage(page_content)
+    books.extend(page.books)
