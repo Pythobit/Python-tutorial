2303bea2a963edcc6bafe0cedaf8955c8b6cdf20
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-20 15:39:45 +0530

    Create book_parser.py

---
+++ Season 11 - Web Scraping with Python/Milestone Project 4 - Book Scraper/Without Logging/parsers/book_parser.py
@@ -0,0 +1,49 @@
+import re
+
+from locators.book_locators import BookLocators
+
+
+class BookParser:
+
+    RATINGS = {
+        'One': 1,
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5
+    }
+
+    def __init__(self, parent):
+        self.parent = parent
+
+    def __repr__(self):
+        return f'Book - {self.name}, £{self.price} ({self.rating} stars)'
+
+    @property
+    def name(self):
+        locator = BookLocators.NAME_LOCATOR
+        item_name = self.parent.select_one(locator).attrs['title']
+        return item_name
+
+    @property
+    def link(self):
+        locator = BookLocators.LINK_LOCATOR
+        item_link = self.parent.select_one(locator).attrs['href']
+        return item_link
+
+    @property
+    def price(self):
+        locator = BookLocators.PRICE_LOCATOR
+        item_price = self.parent.select_one(locator).string
+        pattern = '£([0-9]+\.[0-9]+)'
+        matcher = re.search(pattern, item_price)
+        return float(matcher.group(1))
+
+    @property
+    def rating(self):
+        locator = BookLocators.RATING_LOCATOR
+        star_rating_tag = self.parent.select_one(locator)
+        classes = star_rating_tag.attrs['class']
+        rating_classes = [r for r in classes if r != 'star-rating']
+        rating_number = BookParser.RATINGS.get(rating_classes[0])
+        return rating_number
