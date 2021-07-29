904597eb2437461f293651ea0677cf03917584f6
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-29 10:49:24 +0530

    Create Episode 19 - Async Timeout for Security.py

---
+++ Season 13 - Asynchronous Python Development/Episode 19 - Async Timeout for Security.py
@@ -0,0 +1,29 @@
+import aiohttp
+import async_timeout
+import asyncio
+import time
+
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):   # this won't be needed for google, but for other websites like book scraping.
+        async with session.get(url) as response:
+                print(f'page took {time.time() - page_start}')
+                return response.status
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+loop = asyncio.get_event_loop()
+
+urls = ['http://google.com' for i in range(30)]    # 50 coroutines object
+page_start = time.time()
+loop.run_until_complete(get_multiple_pages(loop, *urls))
+print(f'All took {time.time() - page_start}')
