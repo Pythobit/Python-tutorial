b5593b0a17c4d2b55db73db7aa1ddff13a9e77af
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-07-30 10:16:24 +0530

    Create app.py

---
+++ Season 14 - Web Development with Flask/Episode 04 - Rendering form data with Flask/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+posts = {
+    0: {
+        'post_id': 0,
+        'title': 'Hello World..!!',
+        'content': 'This is my first post on this site.'
+    }
+}
+
+
+@app.route('/')
+def home():
+    return 'Hello world.!'
+
+
+@app.route('/posts/<int:post_id>/')
+def post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return render_template('404.html', message=f'A post with post-id:{post_id}, is not found.!')
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts/form/')
+def form():
+    return render_template('create.html')
+
+
+# 127.0.0.1:5000/posts/create?title=title-here&content=content-here
+@app.route('/posts/create/')
+def create():
+    title = request.args.get('title')
+    content = request.args.get('content')
+    post_id = len(posts)
+    posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+    return redirect(url_for('post', post_id=post_id))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
