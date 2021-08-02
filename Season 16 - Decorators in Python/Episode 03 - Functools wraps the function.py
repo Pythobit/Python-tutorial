7a567e5b8c4c604bd7a185db49d77586520a620b
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-08-02 15:53:26 +0530

    Create Episode 03 - Functools wraps the function.py

---
+++ Season 16 - Decorators in Python/Episode 03 - Functools wraps the function.py
@@ -0,0 +1,25 @@
+import functools    #
+
+
+user = {
+    'username': 'pythobit',
+    'access_level': 'admin'
+}
+
+
+def user_has_permission(func):
+    @functools.wraps(func)      #
+    def secure_func():
+        if user.get('access_level') == 'admin':
+            return func()
+        raise PermissionError
+    return secure_func()
+
+
+@user_has_permission
+def my_function():
+    return 'password for admin is 1244'
+
+
+print(my_function)
+print(my_function.__name__)
